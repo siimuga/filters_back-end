de4efc0649197a79404574314f271e3250006485
Siim Türk <siim.turk@askend.com>
2025-04-27 16:44:04 +0300

    Initial commit

---
+++ src/main/java/com/example/filters_back_end/FiltersBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.filters_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltersBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filters_back_end/FiltersBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filters_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
